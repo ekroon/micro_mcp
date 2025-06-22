73f6bdd5f49bd80ba052e77f4dce7307409b9d80
ekroon <123574+ekroon@users.noreply.github.com>
2025-06-22 20:33:37 +0200

    Update tool_registry.rb

--- lib/micro_mcp/tool_registry.rb
+++ lib/micro_mcp/tool_registry.rb
@@ -7,18 +7,5 @@ module MicroMcp
 
       MicroMcpNative.register_tool(name, description, arguments, block)
     end
-
-    # Delegate schema methods to Schema module for convenience
-    def self.method_missing(method_name, *args, &block)
-      if Schema.respond_to?(method_name)
-        Schema.send(method_name, *args, &block)
-      else
-        super
-      end
-    end
-
-    def self.respond_to_missing?(method_name, include_private = false)
-      Schema.respond_to?(method_name) || super
-    end
   end
 end
